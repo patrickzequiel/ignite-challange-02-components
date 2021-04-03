01ba76f90de80437c9546bc2be1b7ac3f5f20228
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-04-03 17:51:30 -0300

    Problema ao clicar no botão

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,6 +3,7 @@ import { Icon } from './Icon';
 import '../styles/button.scss';
 import { ButtonHTMLAttributes } from 'react';
 
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
